520af1691f957f28b2104cf1886acefa1968135d
Niraj Surve <niraj.surve07@gmail.com>
2024-07-20 11:20:19 +0530

    learn: Object Type

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  type Person = {
+    name: string,
+    age?: number,
+  }
+
+  let person: Person = {
+    name: "Niraj",
+    // age: 22
+  }
+
+  return (
+    <>
+      <h1>Hello {person.name}</h1>
+      <h2>Your age is {person.age}</h2>
+    </>
+  );
+}
+
+export default App;
